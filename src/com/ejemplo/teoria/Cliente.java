3778532729620be1d5b5338981782727c1cebb9e
Daniel Fernandez <daniel.fernandez@campusfp.es>
2022-03-23 10:20:54 +0100

    getters y setters

--- src/com/ejemplo/teoria/Cliente.java
+++ src/com/ejemplo/teoria/Cliente.java
@@ -4,6 +4,34 @@ public class Cliente {
 	String nombre;
 	String ciudad;
 	int facturas;
+	public Cliente(String nombre, String ciudad, int facturas) {
+		super();
+		this.nombre = nombre;
+		this.ciudad = ciudad;
+		this.facturas = facturas;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getCiudad() {
+		return ciudad;
+	}
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
+	public int getFacturas() {
+		return facturas;
+	}
+	public void setFacturas(int facturas) {
+		this.facturas = facturas;
+	}
+	@Override
+	public String toString() {
+		return "Cliente [nombre=" + nombre + ", ciudad=" + ciudad + ", facturas=" + facturas + "]";
+	}
 	
 	
 	
